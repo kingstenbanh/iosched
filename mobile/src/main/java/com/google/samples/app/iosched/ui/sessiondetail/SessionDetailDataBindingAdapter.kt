a016404167d3d10dddd6c4e87e650cefe5896151
Kingsten Banh <kingstenbanh@gmail.com>
2018-11-09 00:55:44 -0500

    Mobile: Add Speaker List

--- mobile/src/main/java/com/google/samples/app/iosched/ui/sessiondetail/SessionDetailDataBindingAdapter.kt
+++ mobile/src/main/java/com/google/samples/app/iosched/ui/sessiondetail/SessionDetailDataBindingAdapter.kt
@@ -1,20 +1,33 @@
 package com.google.samples.app.iosched.ui.sessiondetail
 
 import android.databinding.BindingAdapter
+import android.databinding.DataBindingUtil
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
 import com.google.samples.app.iosched.R
+import com.google.samples.app.iosched.databinding.ItemSpeakerDetailBinding
+import com.google.samples.app.iosched.shared.model.Speaker
 import com.google.samples.app.iosched.shared.model.Tag
+import com.google.samples.app.iosched.shared.util.SpeakerUtils
 
 @BindingAdapter("app:sessionTags")
 fun sessionTags(layout: LinearLayout, sessionTags: List<Tag>) {
     val inf = LayoutInflater.from(layout.context)
     layout.removeAllViews()
     sessionTags.forEach { layout.addView(createSessionTagButton(inf, layout, it)) }
+}
+
+@BindingAdapter("app:sessionSpeakers")
+fun sessionSpeakers(layout: LinearLayout, speakers: Set<Speaker>) {
+    layout.removeAllViews()
+    SpeakerUtils.alphabeticallyOrderedSpeakerList(speakers).forEach {
+        layout.addView(createSessionSpeakerView(layout, it))
+    }
 }
 
 private fun createSessionTagButton(
@@ -26,4 +39,19 @@ private fun createSessionTagButton(
         text = tag.name
         background.setColorFilter(Color.parseColor(tag.color), PorterDuff.Mode.SRC_ATOP)
     }
+}
+
+private fun createSessionSpeakerView(
+    container: ViewGroup,
+    speaker: Speaker
+): View {
+    val binding: ItemSpeakerDetailBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(container.context),
+        R.layout.item_speaker_detail,
+        container,
+        false
+    )
+
+    binding.speaker = speaker
+    return binding.root
 }
