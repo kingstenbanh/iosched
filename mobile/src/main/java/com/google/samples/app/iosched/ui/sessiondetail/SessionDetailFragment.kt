1d12342b46c1be78b0ff8b9b98b17c7e47a2e61c
Kingsten Banh <kingstenbanh@gmail.com>
2018-11-09 16:05:22 -0500

    Session Detail: share menu and intent

--- mobile/src/main/java/com/google/samples/app/iosched/ui/sessiondetail/SessionDetailFragment.kt
+++ mobile/src/main/java/com/google/samples/app/iosched/ui/sessiondetail/SessionDetailFragment.kt
@@ -1,24 +1,26 @@
 package com.google.samples.app.iosched.ui.sessiondetail
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.ShareCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.google.samples.app.iosched.R
 import com.google.samples.app.iosched.databinding.FragmentSessionDetailBinding
 
 class SessionDetailFragment : Fragment() {
+
+    private var shareString = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        setHasOptionsMenu(true)
 
         // TODO: wire up detail page to schedule list and get session ID intent extra (b/72671324)
         val dummySessionId = "1"
@@ -38,6 +40,33 @@ class SessionDetailFragment : Fragment() {
         // TODO: replace with Toolbar (b/73537084)
         (activity as AppCompatActivity).setSupportActionBar(binding.sessionDetailToolbar)
 
+        sessionDetailViewModel.session.observe(this, Observer {
+            shareString = if (it == null) {
+                ""
+            } else {
+                getString(R.string.share_text_session_detail, it.title, it.sessionUrl)
+            }
+        })
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.session_detail_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_share -> {
+                ShareCompat.IntentBuilder.from(activity)
+                    .setType("text/plain")
+                    .setText(shareString)
+                    .setChooserTitle(R.string.intent_chooser_session_detail)
+                    .startChooser()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
